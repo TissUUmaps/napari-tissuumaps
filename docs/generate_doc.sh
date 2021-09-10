6b7e04a188820931d251c1f5ef02cd2331c0698d
Nicolas Pielawski <nicolas@pielawski.fr>
2021-09-10 11:20:33 +0200

    Added documentation generation script.

---
+++ docs/generate_doc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Please install pdoc with `pip install pdoc` prior to running this script
+if ! command -v pdoc &> /dev/null
+then
+    echo "pdoc could not be found."
+    echo "Please make sure pdoc is installed (pip install pdoc)."
+    exit
+fi
+
+pdoc \
+    -o . \
+    -d numpy \
+    --logo https://github.com/wahlby-lab/TissUUmaps/raw/master/misc/design/logo.png \
+    --logo-link https://github.com/wahlby-lab/napari-tissuumaps/ \
+    ../napari_tissuumaps
+
