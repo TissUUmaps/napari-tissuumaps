2ae16562bed20806e132952a521de3de69321356
Nicolas Pielawski <nicolas@pielawski.fr>
2022-08-05 11:10:40 +0200

    Simplified testing procedure.

--- src/napari_tissuumaps/_tests/test_writer.py
+++ src/napari_tissuumaps/_tests/test_writer.py
@@ -1,4 +1,3 @@
-import hashlib
 import tempfile
 from pathlib import Path
 
@@ -36,16 +35,11 @@ def test_writer():
 
         # Images may differ between versions of libraries, so we just check if
         # they exist.
-        assert (tmp_dir / "images/Image.tif").exists()
-        assert (tmp_dir / "labels/Labels.tif").exists()
-        # Check the hashes of the remaining files
-        filehashes = {
-            "main.tmap": "72ef43b7e26c18665f4dda34c9c397b8",
-            "points/Points.csv": "649f61a21544c974a7994aebe06ad3df",
-            "regions/regions.json": "3dba01a0c05a772b0a347cd8fde0af99",
-        }
-        for (filename, true_hash) in filehashes.items():
-            file = open(tmp_dir / filename, "rb")
-            test_hash = hashlib.md5(file.read())
-            file.close()
-            assert test_hash.hexdigest() == true_hash
+        for file in [
+            "main.tmap",
+            "points/Points.csv",
+            "images/Image.tif",
+            "labels/Labels.tif",
+            "regions/regions.json",
+        ]:
+            assert (tmp_dir / file).exists()
